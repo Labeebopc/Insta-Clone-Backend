6c7085568dc3373d39a094b4f3797a2b27635335
Labeeb OPC <Labeebopc313@gmail.com>
2023-01-18 21:53:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors =require('cors')
 const env = require("dotenv").config()
 const mongoose =require('mongoose')
 const postRoutes = require('./routes/postRoutes.js')
-
+const port = process.env.PORT || 5000;
 
 const app =express();
 
@@ -17,10 +17,10 @@ app.use(cors());
 app.use("/api/v1/posts",postRoutes )
 
 const CONNECTION_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 5000;
+
 
 console.log(CONNECTION_URL);
-app.listen(PORT,()=>console.log(`Server running on port :${PORT}`))
+app.listen(port,()=>console.log(`Server running on port :${port}`))
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
 .then(()=>console.log('Database connected Succesfully'))
 .catch((error)=> console.log(error.message));
