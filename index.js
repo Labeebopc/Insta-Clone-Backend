6977e4f0ccdcb305d95398864fe553941859f090
Labeebopc <Labeebopc313@gmail.com>
2023-01-18 18:21:17 +0530

    db details changed

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const postRoutes = require('./routes/postRoutes.js')
 
 const app =express();
 
+
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
-app.use("/api/v1/posts",postRoutes )
 
+app.use("/api/v1/posts",postRoutes )
 
 const CONNECTION_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
 
+console.log(CONNECTION_URL);
 app.listen(PORT,()=>console.log(`Server running on port :${PORT}`))
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
 .then(()=>console.log('Database connected Succesfully'))
